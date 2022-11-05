b4d5a1a74a64d1f534a89c6a0390eeda08e16a20
José Melo <josecoemelo@gmail.com>
2022-11-05 21:30:38 +0000

    improve error message

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const rss = require('rss-url-parser');
 const checkHalt = stock => new Promise(async (resolve, reject) => {
-    if (!stock) { return reject(Error('Stock ticker missing')); }
+    if (!stock) { return reject(Error('Stock symbol required')); }
     if (typeof stock !== 'string') { return reject(Error(`Invalid argument type. Required: string. Found: ${typeof stock}`)); }
 
     const feed = await rss('http://www.nasdaqtrader.com/rss.aspx?feed=tradehalts');
