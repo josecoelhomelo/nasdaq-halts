32e085931df4712edf7973de283063621f317603
José Melo <josecoemelo@gmail.com>
2024-07-31 01:07:58 +0100

    lint file and improve coding standards

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const rss = require('rss-url-parser');
-const checkHalt = ticker => new Promise((resolve, reject) => {
+const parser = require('rss-url-parser');
+const checkHalt = (ticker) => new Promise((resolve, reject) => {
     if (!ticker) { return reject(Error('Stock ticker symbol required')); }
     if (typeof ticker !== 'string') { return reject(Error(`Invalid argument type. Required: string. Found: ${typeof ticker}`)); }
-    rss('http://www.nasdaqtrader.com/rss.aspx?feed=tradehalts')
-        .then((feed) => {       
+    parser('http://www.nasdaqtrader.com/rss.aspx?feed=tradehalts')
+        .then((feed) => {
             const occurrence = feed.find((item) => item.title == ticker);
-            if (occurrence && typeof occurrence['ndaq:resumptiontradetime']['#'] === 'undefined') { return resolve(true); }
-            resolve(false);
+            if (!occurrence || typeof occurrence['ndaq:resumptiontradetime']['#'] !== 'undefined') { return resolve(false); }
+            resolve(true);
         })
         .catch((err) => reject(Error(err)));
 });
